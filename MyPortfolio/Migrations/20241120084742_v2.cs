05f1ac8566ad426e466f74102152817d1f895fe0
Gözde Altun <gozde.altun.iltek@outlook.com>
2024-12-05 08:15:06 +0300

    Add project files.

---
+++ MyPortfolio/Migrations/20241120084742_v2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contects",
+                table: "Contects");
+
+            migrationBuilder.RenameTable(
+                name: "Contects",
+                newName: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "SendTime",
+                table: "Messages",
+                newName: "SendDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts",
+                column: "ContactId");
+
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    ToDoListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.ToDoListId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contacts",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Experiences");
+
+            migrationBuilder.RenameTable(
+                name: "Contacts",
+                newName: "Contects");
+
+            migrationBuilder.RenameColumn(
+                name: "SendDate",
+                table: "Messages",
+                newName: "SendTime");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contects",
+                table: "Contects",
+                column: "ContactId");
+        }
+    }
+}
